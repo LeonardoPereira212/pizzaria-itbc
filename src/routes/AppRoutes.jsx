6aa309da315fff2b51bcc3687969387112e267f5
Leonardo Pereira de Souza <rm99334@estudante.fieb.edu.br>
2026-08-27 10:48:30 -0300

    Atualização do produto concluído

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,53 +1,65 @@
 import {
-    HashRouter,
     BrowserRouter,
+    HashRouter,
     Routes,
     Route
- 
 }
 from "react-router-dom"
+ 
 import HomeFuncionario from "../pages/HomeFuncionario/HomeFuncionario"
 import ListarProduto from "../pages/ListarProduto/ListarProduto"
 import ListarCategoria from "../pages/ListarCategoria/ListarCategoria"
 import NovoProduto from "../pages/NovoProduto/NovoProduto"
+import EditarProduto from "../pages/EditarProduto/EditarProduto"
  
-const AppRoutes = () =>{
+// BrowserRouter : utilize para navegação local ou no link "href"
+// HashRouter: utilize para navegação local ou online utilizando "Link"
  
+const AppRoutes = () => {
  
     return (
-     <HashRouter>
-          <Routes>
-           
-             <Route
-              path="/"
-              element={<HomeFuncionario/>}
-             />
+        <HashRouter>
+            <Routes>
  
-             <Route
-              path="/home"
-              element={<HomeFuncionario/>}
-             />
+                <Route
+                    path="/"
+                    element={<HomeFuncionario/>}
+                />
  
-             <Route
-              path="/produtos"
-              element={<ListarProduto/>}
-             />
-             
-             <Route
-              path="/categorias"
-              element={<ListarCategoria/>}
-             />
-
-              <Route
-              path="/produtos/novo"
-              element={<NovoProduto/>}
-             />
-
-
-          </Routes>
-     </HashRouter>
-         
+                <Route
+                    path="/home"
+                    element={<HomeFuncionario/>}
+                />
+ 
+                <Route
+                    path="/produtos"
+                    element={<ListarProduto/>}
+                />
+ 
+                <Route
+                    path="/categorias"
+                    element={<ListarCategoria/>}
+                />
+ 
+                <Route
+                    path="/produtos/novo"
+                    element={<NovoProduto/>}
+                />
+ 
+                <Route
+                    path="/produtos/novo/id"
+                    element={<NovoProduto/>}
+                />
+ 
+                <Route
+                    path="/produtos/editar/:id"
+                    element={<EditarProduto/>}
+                />
+ 
+            </Routes>
+        </HashRouter>
     )
 }
  
 export default AppRoutes
+ 
